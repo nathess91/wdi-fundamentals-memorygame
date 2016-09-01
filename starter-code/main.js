3f0404f135ef86c79c810c166543fa87574f5398
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-08-31 18:14:53 -0700

    updated js file

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,7 +7,7 @@ var cardFour = "king";
 
 // Based on whether or not user discovers a match, notify them with a winning or losing message 
 
-// Queen matches
+
 if (cardOne === cardTwo) {
 	alert("You found a match!");
 } else if (cardTwo === cardOne) {
