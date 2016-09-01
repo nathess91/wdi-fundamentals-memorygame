eb6d0605b465681b1555e1d3fc6042ccdafd66f8
Natalia Hess <nhess@natalia-hesss-macbook-pro.local>
2016-08-31 18:12:42 -0700

    updated game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,18 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+// Based on whether or not user discovers a match, notify them with a winning or losing message 
+
+// Queen matches
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else if (cardTwo === cardOne) {
+	alert("You found a match!");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} else if (cardFour === cardThree) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
